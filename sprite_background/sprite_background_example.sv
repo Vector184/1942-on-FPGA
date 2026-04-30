8e6a617d5d9baba14abbf98d1cfa06d82a4d339a
BigBraveGoldfish <kakkar3@illinois.edu>
2026-04-30 18:02:12 -0500

    Revert

--- sprite_background/sprite_background_example.sv
+++ sprite_background/sprite_background_example.sv
@@ -5,7 +5,7 @@ module sprite_background_example (
 	output logic [3:0] red, green, blue
 );
 
-logic [17:0] rom_address;
+logic [18:0] rom_address;
 logic [3:0] rom_q;
 
 logic [3:0] palette_red, palette_green, palette_blue;
@@ -17,7 +17,7 @@ assign negedge_vga_clk = ~vga_clk;
 
 // address into the rom = (x*xDim)/640 + ((y*yDim)/480) * xDim
 // this will stretch out the sprite across the entire screen
-assign rom_address = ((DrawX * 256) / 640) + (((DrawY * 979) / 480) * 256);
+assign rom_address = ((DrawX * 256) / 640) + (((DrawY * 1188) / 480) * 256);
 
 always_ff @ (posedge vga_clk) begin
 	red <= 4'h0;
--- sprite_background/sprite_background_rom.sv
+++ sprite_background/sprite_background_rom.sv
@@ -1,10 +1,10 @@
 module sprite_background_rom (
 	input logic clock,
-	input logic [17:0] address,
+	input logic [18:0] address,
 	output logic [3:0] q
 );
 
-logic [3:0] memory [0:250623] /* synthesis ram_init_file = "./sprite_background/sprite_background.COE" */;
+logic [3:0] memory [0:304127] /* synthesis ram_init_file = "./sprite_background/sprite_background.COE" */;
 
 always_ff @ (posedge clock) begin
 	q <= memory[address];
